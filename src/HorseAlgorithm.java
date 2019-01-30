01e0c44173e2cb6b3854ee97090c9b823b0ed3b7
Donny Lee <korlee1986@gmail.com>
2019-01-30 18:01:20 +0100

    Initial commit

---
+++ src/HorseAlgorithm.java
@@ -0,0 +1,13 @@
+
+
+
+public class HorseAlgorithm {
+
+    HorseAlgorithm () {
+        System.out.println("Implementation");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Henlo, World!");
+    }
+}
